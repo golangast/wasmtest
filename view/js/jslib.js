4e16f3ef395e88924bb2bd7e97ba1f213a2d4974
zach endrulat <golangast@gmail.com>
2024-04-08 20:52:24 -0500

    added forms component

--- view/js/jslib.js
+++ view/js/jslib.js
@@ -19,3 +19,68 @@ function CreateComponent(ele, html) {
 
     customElements.define(ele, Custom);
 };
+
+//https://kinsta.com/blog/web-components/
+function CreateFormComponent(ele, html) {
+
+(() => {
+
+  // stop form submission and output field names/values to console
+  const form = document.getElementById(ele);
+
+  form.addEventListener('submit', e => {
+    
+    e.preventDefault();
+    console.clear();
+    console.log('Submitted data:');
+
+    const data = new FormData(form);
+    for (let nv of data.entries()) {
+      console.log(`  ${ nv[0] }: ${ nv[1] }`);
+    }
+
+  });
+
+})();
+
+
+// web component
+class Custom extends HTMLElement {
+
+  static formAssociated = true;
+  
+  formAssociatedCallback(form) {
+    console.log('form associated:', form.id);
+  }
+  
+  constructor() {
+    super();
+    this.internals = this.attachInternals();
+    this.setValue('');
+  }
+  
+  // connect component
+  connectedCallback() {
+    
+    const shadow = this.attachShadow({ mode: 'closed' });
+    shadow.innerHTML = html;
+    
+    // monitor input values
+    shadow.querySelector('input').addEventListener('change', e => {
+      this.setValue(e.target.value);
+    });
+    
+  }
+  
+  // set form value
+  setValue(v) {
+    this.value = v;
+    this.internals.setFormValue(v);
+  }
+  
+}
+
+// register component
+customElements.define( ele, Custom );
+
+}
--- view/js/main.go
+++ view/js/main.go
@@ -6,8 +6,16 @@ import (
 
 func main() {
 	temp := `<p>dfsfdfdf</p>`
+	tempform := `  <form action="/" method="post">
+	<label for="fname">First name:</label><br>
+	<input type="text" id="fname" name="fname" value="John"><br>
+	<label for="lname">Last name:</label><br>
+	<input type="text" id="lname" name="lname" value="Doe"><br><br>
+	<input type="submit" value="Submit">
+  </form> `
 
 	js.Global().Call("CreateComponent", "my-component", temp)
+	js.Global().Call("CreateFormComponent", "form-component", tempform)
 
 	<-make(chan bool)
 }
